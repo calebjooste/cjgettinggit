461bf24dc9b26a312c849920101675dfe9f84ab8
calebjooste <69714925+calebjooste@users.noreply.github.com>
2020-10-20 21:26:39 +0200

    Zola

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace New_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
